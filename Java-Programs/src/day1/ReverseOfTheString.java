1fae36f0555b19825de87d5d625e149c9eb3ffb3
Raza Pathan <razafn@gmail.com>
2025-04-12 13:58:18 +0530

    Raza | added the Reverse String Code.

--- Java-Programs/src/day1/ReverseOfTheString.java
+++ Java-Programs/src/day1/ReverseOfTheString.java
@@ -1,10 +1,28 @@
 package day1;
 
+import java.util.Scanner;
+
 public class ReverseOfTheString {
 
 	public static void main(String[] args) {
 
-		System.out.println("Raza");
+		System.out.println("W.A.P to reverse the String which is given by user at runtime!!!");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the String which you want to reverse!");
+		String input = scan.next();
+		System.out.println(input);
+		int size = input.length();
+		String reverse = "";
+		while(size > 0) {
+			
+			char ch = input.charAt(size - 1);
+			reverse = reverse + ch;
+			size--;
+		}
+		
+		System.out.println("Reverse String is "+reverse);
+		scan.close();
+
 	}
 
 }
