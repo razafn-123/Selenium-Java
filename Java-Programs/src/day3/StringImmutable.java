d143d6855a246a8f26588e1fad9a9de601b13261
Raza Pathan <razafn@gmail.com>
2025-04-14 18:32:50 +0530

    Raza | added the string immutable program in day3 folder.

---
+++ Java-Programs/src/day3/StringImmutable.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class StringImmutable {
+
+	public static void main(String[] args) {
+		
+		String sentence = "Hello World!";
+		
+		sentence.concat("Welcome to Java Programming");
+		
+		System.out.println(sentence);//Strings are immutable in java!!!
+	}
+}
